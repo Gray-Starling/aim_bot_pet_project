cefffa4a30df7bb71f84bd43aac28766e1b24cd6
Sergey <skvorcovsa90@gmail.com>
2022-07-24 17:18:56 +1000

    [feature]: create app

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import { GameBoard } from './gameBoard/GameBoard'
+
+function App() {
+  return (
+    <div className="main">
+      <GameBoard />
+    </div>
+  )
+}
+
+export default App
