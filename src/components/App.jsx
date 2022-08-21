959b32c844369840f5840b87f615e7d2f871e2f3
Sergey <skvorcovsa90@gmail.com>
2022-08-21 17:01:51 +1000

    refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
 import './App.scss'
-import { GameBoard } from './gameBoard/GameBoard'
 
 function App() {
   return (
     <div className="main">
       <h1>Aim training</h1>
-      <GameBoard />
     </div>
   )
 }
